76029cc5798638718a58050dde3c1027477fe05e
Alex <alexdthi@gmail.com>
2024-09-14 18:23:23 -0400

    Added start of API call to zillowAPI

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,21 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import http.client
+
+conn = http.client.HTTPSConnection("zillow56.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "64d06c0a70mshc1395b98bc739c1p1e5c27jsnc75a73d509d8",
+    'x-rapidapi-host': "zillow56.p.rapidapi.com"
+}
+
+print(data.decode("utf-8"))
 
 app = Flask(__name__)
 cors = CORS(app, origins='*')  # Allow cross-origin 
 
 @app.route('/', methods=['GET'])
-def users():
+def users(city):
     return jsonify(
         {
             'users': [
@@ -15,6 +25,10 @@ def users():
             ]
         }
     )
+    conn.request("GET", f"/search?location={city}&output=json&status=forSale&sortSelection=priorityscore&listing_type=by_agent&doz=any", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
 
 if __name__ == '__main__':
     app.run(debug=True, port="8080")
